2bb6659893217e02efaa4d9eb146ec02dc68d491
fassma89@gmail.com <fassma89@gmail.com>
2023-04-28 16:10:33 +0200

    removeToDo.js modified

--- assets/js/modules/removeToDo.js
+++ assets/js/modules/removeToDo.js
@@ -2,4 +2,7 @@ export function removeToDo(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
     LIST[element.id].trash = true;
 
+
+    localStorage.setItem("TODO", JSON.stringify(LIST));
   }
+  
